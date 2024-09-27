db23cf93b7b7c28f89c5ee0c556223bb047d7bca
Jisatsu13 <159918810+Jisatsu13@users.noreply.github.com>
2024-09-27 18:36:29 +0300

    Update i2c_help_fun.h

--- i2c_help_fun.h
+++ i2c_help_fun.h
@@ -9,5 +9,13 @@
 
 void I2C_WriteData (I2C_TypeDef *I2Cx,uint8_t Address, uint8_t Reg, uint8_t Data);
 void I2C_ReadData (I2C_TypeDef *I2Cx, uint8_t Address, uint8_t Reg, uint8_t *buffer, uint8_t size);
+void I2C_MultyWriteData(I2C_TypeDef *I2Cx, uint8_t Address, uint8_t Reg, uint8_t *Data, uint16_t size);
+
+void I2C_Start (I2C_TypeDef *I2Cx);
+void I2C_Write (I2C_TypeDef *I2Cx, uint8_t data);
+void I2C_MultyWrite (I2C_TypeDef *I2Cx, uint8_t *data, uint16_t size);
+void I2C_Read (I2C_TypeDef *I2Cx, uint8_t Address, uint8_t *buffer, uint8_t size);
+void I2C_Address (I2C_TypeDef *I2Cx,uint8_t Address);
+void I2C_Stop (I2C_TypeDef *I2Cx);
 
 #endif
